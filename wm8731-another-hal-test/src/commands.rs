c345268b42a6c6facd95c9c56dfc0e63e9eee2ec
YruamaLairba <Yruama_Lairba@hotmail.com>
2022-03-03 20:38:29 +0100

    test crate improve refactoring through macro

--- wm8731-another-hal-test/src/commands.rs
+++ wm8731-another-hal-test/src/commands.rs
@@ -2,6 +2,21 @@ use rtic::Mutex;
 use rtt_target::rprintln;
 use wm8731_another_hal::prelude::*;
 
+/// for cmd taking a bool as parameter
+macro_rules! bool_cmd {
+    ($name:ident, $set_name:ident) => {
+        pub fn $name<I: WriteFrame>(mut wm8731: impl Mutex<T = Wm8731<I>>, val: &str) {
+            let val = val.trim();
+            if let Ok(val) = val.parse::<bool>() {
+                wm8731.lock(|wm8731| {
+                    wm8731.$set_name(val);
+                });
+                rprintln!(concat!(stringify!($name), " {:?}"), val);
+            }
+        }
+    };
+}
+
 pub fn hpvol<I: WriteFrame>(mut wm8731: impl Mutex<T = Wm8731<I>>, val: &str) {
     let val = val.trim();
     if val.is_empty() {
@@ -28,32 +43,6 @@ pub fn insel<I: WriteFrame>(mut wm8731: impl Mutex<T = Wm8731<I>>, val: &str) {
     rprintln!("insel {:?}", val);
 }
 
-pub fn dacsel<I: WriteFrame>(mut wm8731: impl Mutex<T = Wm8731<I>>, val: &str) {
-    let val = val.trim();
-    if let Ok(val) = val.parse::<bool>() {
-        wm8731.lock(|wm8731| {
-            wm8731.set_dacsel(val);
-        });
-        rprintln!("dacsel {:?}", val);
-    }
-}
-
-pub fn bypass<I: WriteFrame>(mut wm8731: impl Mutex<T = Wm8731<I>>, val: &str) {
-    let val = val.trim();
-    if let Ok(val) = val.parse::<bool>() {
-        wm8731.lock(|wm8731| {
-            wm8731.set_bypass(val);
-        });
-        rprintln!("bypass {:?}", val);
-    }
-}
-
-pub fn dacmu<I: WriteFrame>(mut wm8731: impl Mutex<T = Wm8731<I>>, val: &str) {
-    let val = val.trim();
-    if let Ok(val) = val.parse::<bool>() {
-        wm8731.lock(|wm8731| {
-            wm8731.set_dacmu(val);
-        });
-        rprintln!("dacmu {:?}", val);
-    }
-}
+bool_cmd!(dacsel, set_dacsel);
+bool_cmd!(bypass, set_bypass);
+bool_cmd!(dacmu, set_dacmu);
